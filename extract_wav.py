1b0a25e7b27f461f307d3f22e5bd5a963b112e07
yuangan <411246903@qq.com>
2024-03-10 16:38:10 +0800

    extract wav

--- extract_wav.py
+++ extract_wav.py
@@ -2,10 +2,11 @@ from glob import glob
 import os
 from tqdm import tqdm
 
-allmp4s = glob('/data3/lipread_test_25/video/*.mp4')
+root_lrw = '/data3/lipread_test_25/video/'
+allmp4s = glob(f'{root_lrw}/*.mp4')
 path_wav='./talking_head_testing/lipread_test_wav/'
 os.makedirs(path_wav, exist_ok=True)
 
 for mp4 in tqdm(allmp4s):
     name = os.path.basename(mp4)
-    os.system(f'ffmpeg -loglevel error -y -i /data3/lipread_test_25/video/{name} {path_wav}/{name[:-4]}.wav')
+    os.system(f'ffmpeg -loglevel error -y -i {root_lrw}/{name} {path_wav}/{name[:-4]}.wav')
