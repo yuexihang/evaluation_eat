69f6ac3e0ad80bf8af113a86c1f9d5f89ac19e93
yuexihang <xihangyue@gmail.com>
2024-03-10 16:44:05 +0800

    update lrw gt preprocessing.

--- preprocess_lrw_gt.sh
+++ preprocess_lrw_gt.sh
@@ -1,5 +1,5 @@
 VIDEO_PATH=${1} # eg. /data3/lipread_test_25/video
 
 cd './code'
-python preprocess_lrw.py --save_name lrw_gt --fake_pth ${VIDEO_PATH} --name_mode 1 #--ours_filter_100
+python preprocess_lrw.py --save_name lrw_gt --fake_pth ${VIDEO_PATH} --name_mode 1 --need_align_crop #--ours_filter_100
 
